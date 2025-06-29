c5b8943a5bf0069bf032f271d72df00aaedbbfb6
Nilesh Parab <145212646+nileshparab08@users.noreply.github.com>
2025-06-29 05:43:52 +0530

    Update App.controller.js
    Added Vendor and Amount fields

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -12,9 +12,9 @@ sap.ui.define([
     onInit: function() {
       var oData = {
         PurchaseOrders: [
-          { PO_ID: "PO123456", Status: "Pending" },
-          { PO_ID: "PO123457", Status: "Approved" },
-          { PO_ID: "PO123458", Status: "Rejected" }
+          { PO_ID: "PO123456", Status: "Pending", Vendor: "TCS", Amount: "1,200 EUR", CreatedOn: "2024-04-05" },
+          { PO_ID: "PO123457", Status: "Approved", Vendor: "Wipro", Amount: "1,300 EUR", CreatedOn: "2024-04-10" },
+          { PO_ID: "PO123458", Status: "Rejected", Vendor: "Infosys", Amount: "1,100 EUR", CreatedOn: "2024-04-15" }
         ]
       };
       var oModel = new JSONModel(oData);
